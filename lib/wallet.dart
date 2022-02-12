069716ca7e7d6ca0de143085665cf3bd8d18eb8d
Stefano Fornari <stefano.fornari@gmail.com>
2022-02-12 17:46:02 +0100

    get a wallet's balance from the ETH network

--- lib/wallet.dart
+++ lib/wallet.dart
@@ -3,16 +3,16 @@ import 'package:big_decimal/big_decimal.dart';
 class EasyWallet {
   final String address;
 
-  BigDecimal _wallet;
+  BigDecimal _balance;
 
-  EasyWallet(this.address) : _wallet = BigDecimal.parse("0.0");
+  EasyWallet(this.address) : _balance = BigDecimal.parse("0.0");
 
   BigDecimal get balance {
-    return _wallet;
+    return _balance;
   }
 
   EasyWallet withBalance(BigDecimal balance) {
-    _wallet = BigDecimal.parse("10.0");
+    _balance = balance;
     return this;
   }
 
--- test/wallet_manager_test.dart
+++ test/wallet_manager_test.dart
@@ -1,9 +1,20 @@
+import 'dart:convert';
+
 import 'package:test/test.dart';
 
+import 'package:big_decimal/big_decimal.dart';
 import 'package:easy_wallet/wallet.dart';
 import 'package:easy_wallet/wallet_manager.dart';
+import 'package:http/http.dart';
+
+import 'mock_client.dart';
+
+const String WALLET1 = "0x00000000219ab540356cBB839Cbe05303d7705Fa";
+const String WALLET2 = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2";
+
 
 void main() {
+  
   test('construc_wallet_manager', () {
     expect(WalletManager("https://mainnet.infura.io/v3/PROJECTID1").endpoint,
       "https://mainnet.infura.io/v3/PROJECTID1");
@@ -12,15 +23,37 @@ void main() {
   });
 
   test('get_wallet_balance', () async {
-    EasyWallet w = EasyWallet("0x00000000219ab540356cBB839Cbe05303d7705Fa");
-    WalletManager wm = WalletManager("https://mainnet.infura.io/v3/PROJECTID1");
+    EasyWallet w = EasyWallet(WALLET1);
+    WalletManager wm = WalletManageWithMock("https://a.endpoint.io/v3/PROJECTID1");
 
-    print("Getting balance");
     await wm.balance(w);
-    print("done");
+    expect(w.balance, BigDecimal.parse("9343922000069000000000069"));
 
-    //print(balance.getValueInUnit(EtherUnit.ether));
-
-
+    w = EasyWallet(WALLET2);
+    await wm.balance(w);
+    expect(w.balance, BigDecimal.parse("7592901870686660123035923"));
   });
+}
+
+// -------------------------------------------------------- WalletManageWithMock
+
+class WalletManageWithMock extends WalletManager {
+  WalletManageWithMock(endpoint) : super(endpoint);
+
+  @override
+  Client getHttpClient() {
+    return MockClient((method, data) {
+      if (method == "eth_getBalance") {
+        if (data != null) {
+          List args = data as List;
+          if (args.first as String == WALLET1.toLowerCase()) {
+            return "0x7baa706cf4a4220055045";
+          } else if (args.first as String == WALLET2.toLowerCase()) {
+            return "0x647DC0901C745DB420913";
+          }
+        }
+      }
+      return "boh";
+    });
+  }
 }
