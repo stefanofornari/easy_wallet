c3555e081adae8bdf8ce6a5c88fa5a96b0fde0aa
Stefano Fornari <stefano.fornari@gmail.com>
2022-02-23 23:26:07 +0100

    added input validation for wallet address

--- lib/wallet.dart
+++
@@ -1,9 +0,0 @@
-import 'package:big_decimal/big_decimal.dart';
-
-class EasyWallet {
-  final String address;
-
-  BigDecimal balance;
-
-  EasyWallet(this.address) : balance = BigDecimal.parse("0.0");
-}
